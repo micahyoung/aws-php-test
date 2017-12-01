22d700493814abe4e4184d4d6833b3ba9374aea3
Micah Young <myoung@pivotal.io>
2017-12-01 13:50:26 -0500

    Commment out local run

--- up.sh
+++ up.sh
@@ -17,9 +17,9 @@ fi
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 
-pushd aws-php-sample/
-  php --php-ini ../php.ini sample.php
-popd
+#pushd aws-php-sample/
+#  php --php-ini ../php.ini sample.php
+#popd
 
 pushd aws-php-sample/
   cf push aws-php-sample --no-start -c 'php /home/vcap/app/htdocs/sample.php' --health-check-type process -b php_buildpack --no-route
